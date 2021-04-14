a73baf514a8e7f6dffd57c55a0d75df33ac82651
alexandersaiko25 <alexandersaiko25@gmail.com>
2021-04-14 06:20:21 +0300

    Add files via upload

---
+++ OOP_2_2.py
@@ -0,0 +1,23 @@
+class Rectangle:
+
+    def __init__(self):
+        pass
+
+    def side(self):
+        print("Сторона прямоугольника")
+
+    def corner(self):
+        pass
+
+class Figure:
+
+    def figures(self):
+        print("Метод класса Фигура")
+
+class Clicker:
+
+    def mouse_clicker(self):
+        print("Кликнули мышью. Кнопка нажата")
+
+obj = Clicker()
+obj.mouse_clicker()
